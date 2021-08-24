02ffcdf1ac9fb53e89b033c18e921ba0d36ba794
HugoHernandez06 <hugoa.hernandez@upb.edu.co>
2021-08-24 15:22:46 -0700

    Feature1
    Configuracion main.c y creacion de la funcion feature1

--- main.c
+++ main.c
@@ -3,6 +3,26 @@
 #include "student.h"
 
 int main(int argc, char *argv[]){
+    if(argc !=3){
+        printf("Numero incorrecto de archivos\n");
+    }
 
+    FILE *fin = fopen(argv[1],"r+");
+    if(fin == NULL){
+        perror("Error al abrir archivo de entrada: \n");
+        return (EXIT_FAILURE);
+    }
+
+    FILE *fout = fopen(argv[2],"w+");
+    if(fout == NULL){
+        perror("Error al abrir archivo de salida: \n");
+        return (EXIT_FAILURE);
+    }
+    
+    feature1(fin,fout);
+
+    fclose(fin);
+    fflush(fout);
+    fclose(fout);
     return EXIT_SUCCESS;
 }
--- student.c
+++ student.c
@@ -1,3 +1,11 @@
 #include <stdlib.h>
 #include "student.h"
+#define MAX 1000
 
+void feature1(FILE *fin, FILE *fout){
+    char buffer[MAX];         
+    fgets(buffer, MAX, fin); 
+    strtok(buffer, "\n");
+    printf("La Primera línea fin es: '%s'\n", buffer);
+    fputs(buffer,fout);
+}
