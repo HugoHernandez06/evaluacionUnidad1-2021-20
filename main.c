83ecffbc4220e56701e5c889f4c752d158e72c32
HugoHernandez06 <hugoa.hernandez@upb.edu.co>
2021-08-24 16:33:57 -0700

    Feature2

--- main.c
+++ main.c
@@ -20,6 +20,7 @@ int main(int argc, char *argv[]){
     }
     
     feature1(fin,fout);
+    feature2(fin,fout);
 
     fclose(fin);
     fflush(fout);
--- student.c
+++ student.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "student.h"
+#include <string.h>
 #define MAX 1000
 
 void feature1(FILE *fin, FILE *fout){
@@ -7,5 +8,23 @@ void feature1(FILE *fin, FILE *fout){
     fgets(buffer, MAX, fin); 
     strtok(buffer, "\n");
     printf("La Primera línea fin es: '%s'\n", buffer);
+    fputs(buffer,fout);
+}
+
+void feature2(FILE *fin, FILE *fout){
+    char buffer[MAX];
+    fgets(buffer, MAX, fin);
+    strtok(buffer, "\n");
+	printf("Frase archivo fin: %s\n", buffer);
+	
+	int leng = strlen(buffer);
+    for (int i = 0; i < leng/2; i++)
+    {   
+        char tmp = buffer[i];
+        buffer[i] = buffer[leng - i - 1];
+        buffer[leng - i - 1] = tmp;
+    }
+	printf("Frase Invertida: %s\n", buffer);
+    fputs("\n",fout);
     fputs(buffer,fout);
 }
