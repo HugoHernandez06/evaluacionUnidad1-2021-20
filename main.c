7dcc0b9fef73f4b8cc51d736c637f2d4443ac565
HugoHernandez06 <hugoa.hernandez@upb.edu.co>
2021-09-02 23:06:58 -0700

    Actualizacion main.c

--- main.c
+++ main.c
@@ -3,38 +3,54 @@
 #include "student.h"
 
 int main(int argc, char *argv[]){
-    int length = 0;
 
-    if(argc !=3){
-        printf("Numero incorrecto de archivos\n");
+    if(argc != 3){
+        printf("number of arguments is not 3\n");
+        return(EXIT_FAILURE);
     }
 
-    FILE *fin = fopen(argv[1],"r+");
-    if(fin == NULL){
-        perror("Error al abrir archivo de entrada: \n");
-        return (EXIT_FAILURE);
+    FILE *fin = fopen(argv[1],"r");
+    if (fin == NULL){
+        perror("open fin file fails: ");
+        return(EXIT_FAILURE);
     }
 
-    FILE *fout = fopen(argv[2],"w+");
-    if(fout == NULL){
-        perror("Error al abrir archivo de salida: \n");
-        return (EXIT_FAILURE);
+    FILE *fout = fopen(argv[2],"w");
+    if (fout == NULL){
+        perror("open fout file fails: ");
+        return(EXIT_FAILURE);
     }
-    
+
     feature1(fin,fout);
     feature2(fin,fout);
-    feature3(fin,fout);  
-    struct Obj_t pobj;
-    feature6(fin, &pobj);
-    feature7(fout, &pobj);
+    feature3(fin,fout);
 
-    struct _courseInfo_t *pobj1;
-    feature8(fin, &pobj1,&length);
-    feature9(fout, pobj1,length);
+    int *parr = NULL;
+    int length = 0;
+    char *op = NULL;
+    feature4(fin,&parr,&length,&op);
+    feature5(fout, parr, length, op);
 
+    struct Obj_t obj = {"test",123};
+    feature6(fin, &obj);
+    feature7(fout,&obj);
 
+    struct _courseInfo_t *pobj;
+    int objArrSize = 0;
+    feature8(fin, &pobj, &objArrSize);
+    feature9(fout, pobj,objArrSize);
+    
+    // feature10: release memory
+/*
+    free(parr);
+    free(op);
+    free(obj.nombre);
+    free(pobj);
     fclose(fin);
     fflush(fout);
     fclose(fout);
+*/
+
     return EXIT_SUCCESS;
+
 }
